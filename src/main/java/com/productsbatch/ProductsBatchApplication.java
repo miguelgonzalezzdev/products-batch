a9237f6134cae0a57420cc419ec307410890c72a
miguelgonzalezzdev <miguelgonzalezdev@gmail.com>
2025-12-26 15:27:05 +0100

    first commit

---
+++ src/main/java/com/productsbatch/ProductsBatchApplication.java
@@ -0,0 +1,13 @@
+package com.productsbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductsBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductsBatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/productsbatch/ProductsBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.productsbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductsBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
